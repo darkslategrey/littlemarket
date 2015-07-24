b46a2ee8144b9ac04c4383f4b18b363651ef6b20
Darkslategrey <dslategrey@gmail.com>
2015-07-24 10:52:05 +0200

    rattrapage

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150722065442) do
+
+  create_table "creations", force: :cascade do |t|
+    t.integer  "lm_id"
+    t.string   "imgs"
+    t.string   "categs"
+    t.string   "title"
+    t.string   "subtitle"
+    t.text     "desc"
+    t.string   "tags"
+    t.string   "materials"
+    t.string   "colors"
+    t.string   "styles"
+    t.string   "events"
+    t.string   "dest"
+    t.string   "prices"
+    t.string   "deliveries"
+    t.string   "options"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.string   "state",      default: "published"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "check_little_ids", default: false
+  end
 
 end
